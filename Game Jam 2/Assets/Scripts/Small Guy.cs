29dec7511602e18d1ab76f91f807cd35b3b2e8a6
bla-bla500 <hermannc125@gmail.com>
2026-02-17 07:36:57 -0500

    Projectiles shoot

--- Game Jam 2/Assets/Scripts/Small Guy.cs
+++ Game Jam 2/Assets/Scripts/Small Guy.cs
@@ -1,12 +1,30 @@
+using System.Collections;
 using UnityEngine;
 
 public class SmallGuy : MonoBehaviour
 {
     [SerializeField] Camera cam;
+    [SerializeField] GameObject projectile;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            StartCoroutine("Shoot");
+        }   
+    }
     void LateUpdate()
     {
-        Debug.Log(cam.ScreenToWorldPoint(Input.mousePosition));
         gameObject.transform.position = cam.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0,0,10);
-
+    }
+    
+    public IEnumerator Shoot()
+    {
+        while (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("here");
+            Instantiate(projectile, gameObject.transform.position, gameObject.transform.rotation);
+            yield return new WaitForSeconds(0.2f);
+        }
     }
 }
